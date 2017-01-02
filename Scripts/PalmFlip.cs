1d504ac66df6b3f2c34a189008c528c470554c3d
Olutade Ajiboye <mrbigheaddd@gmail.com>
2017-01-02 23:49:41 +0100

    chore: brought out script folder
    The scripts that contained the gesture detections were brought out to a new folder in the root folder of the directory. This is to make it easy for people to access them.

---
+++ Scripts/PalmFlip.cs
@@ -0,0 +1,162 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+using Leap.Unity.Attributes;
+using Leap.Unity;
+
+namespace CustomLeapGestures
+{
+	public class PalmFlip : Detector
+	{
+		[Tooltip("The interval in seconds at which to check this detector's conditions.")]
+		public float period = .1f;
+
+		[Tooltip("The maximum amount of time in seconds allowed to be used to flip palm. ")]
+		public float maximumFlipTime = .1f;
+
+		[AutoFind(AutoFindLocations.Parents)]
+		[Tooltip("The hand model to watch. Set automatically if detector is on a hand.")]
+		public IHandModel handModel = null;
+
+		[Tooltip("The angle in degrees from the palm down direction at which to turn on.")]
+		[Range(0, 360)]
+		public float onAngleDown = 45;
+
+		[Tooltip("The angle in degrees from the palm down direction at which to turn off.")]
+		[Range(0, 360)]
+		public float offAngleDown = 65;
+
+		[Tooltip("The angle in degrees from the palm up direction at which to turn on.")]
+		[Range(0, 360)]
+		public float onAngleUp = 45;
+
+		[Tooltip("The angle in degrees from the palm up direction at which to turn off.")]
+		[Range(0, 360)]
+		public float offAngleUp = 65;
+
+		float flipTime = 0;
+		bool isPalmFlipping = false;
+		bool isPalmDown = false;
+
+		private IEnumerator watcherCoroutine;
+
+		private void OnValidate()
+		{
+			if (offAngleUp < onAngleUp)
+			{
+				offAngleUp = onAngleUp;
+			}
+			if (offAngleDown < onAngleDown)
+			{
+				offAngleDown = onAngleDown;
+			}
+		}
+
+		private void Awake()
+		{
+			watcherCoroutine = PalmWatcher();
+		}
+
+		private void OnEnable()
+		{
+			StartCoroutine(watcherCoroutine);
+		}
+
+		private void OnDisable()
+		{
+			StopCoroutine(watcherCoroutine);
+			Deactivate();
+		}
+
+		private IEnumerator PalmWatcher()
+		{
+			Hand hand;
+			Vector3 normal;
+			bool isPalmUp = false;
+			float angleToDown;
+			float angleToUp;
+			while (true)
+			{
+				if (handModel != null)
+				{
+					hand = handModel.GetLeapHand();
+					if (hand != null)
+					{
+						normal = hand.PalmNormal.ToVector3();
+						angleToDown = Vector3.Angle(normal, Vector3.down);
+						angleToUp = Vector3.Angle(normal, Vector3.up);
+						if (angleToDown <= onAngleDown)
+						{
+							isPalmDown = true;
+						}
+						else if (angleToDown > offAngleDown)
+						{
+							if (isPalmDown)
+							{
+								isPalmFlipping = true;
+							}
+							isPalmDown = false;
+						}
+
+						if (isPalmFlipping)
+						{
+							flipTime += 1 * Time.deltaTime;
+							normal = hand.PalmNormal.ToVector3();
+							if (angleToUp <= onAngleUp && flipTime <= maximumFlipTime)
+							{
+								Activate();
+								flipTime = 0;
+								isPalmFlipping = false;
+								isPalmUp = true;
+								Debug.Log("I just flipped palm");
+							}
+						}
+						if (angleToUp > offAngleUp && isPalmUp)
+						{
+							Deactivate();
+							isPalmUp = false;
+							Debug.Log("I removed palm from up-position");
+						}
+						if (flipTime >= maximumFlipTime)
+						{
+							flipTime = 0;
+							isPalmFlipping = false;
+						}
+					}
+				}
+				yield return new WaitForSeconds(period);
+			}
+		}
+
+#if UNITY_EDITOR
+		private void OnDrawGizmos()
+		{
+			if (ShowGizmos && handModel != null)
+			{
+				Color centerColor;
+				if (IsActive)
+				{
+					centerColor = Color.green;
+				}
+				else if (isPalmDown)
+				{
+					centerColor = Color.green;
+				}
+				else {
+					centerColor = Color.red;
+				}
+				Hand hand = handModel.GetLeapHand();
+				if (!isPalmFlipping)
+				{
+					Utils.DrawCone(hand.PalmPosition.ToVector3(), hand.PalmNormal.ToVector3(), onAngleDown, hand.PalmWidth, centerColor, 8);
+					Utils.DrawCone(hand.PalmPosition.ToVector3(), hand.PalmNormal.ToVector3(), offAngleDown, hand.PalmWidth, Color.blue, 8);
+				}
+				else {
+					Utils.DrawCone(hand.PalmPosition.ToVector3(), hand.PalmNormal.ToVector3(), onAngleUp, hand.PalmWidth, centerColor, 8);
+					Utils.DrawCone(hand.PalmPosition.ToVector3(), hand.PalmNormal.ToVector3(), offAngleUp, hand.PalmWidth, Color.blue, 8);
+				}
+			}
+		}
+#endif
+	}
+}
---
+++ Scripts/Wave.cs
@@ -0,0 +1,196 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+using Leap.Unity;
+using Leap.Unity.Attributes;
+
+namespace CustomLeapGestures
+{
+	public class Wave : Detector
+	{
+		[Tooltip("The interval in seconds at which to check this detector's conditions.")]
+		public float period = .1f;
+
+		[Tooltip("The angle in degrees of the fingers' pointing direction from the up direction in which to begin wave")]
+		[Range(0, 360)]
+		public float handOnAngle = 30f;
+
+		[Tooltip("The angle in degrees of the fingers' point direction from the up direction in which to start tracking wave")]
+		[Range(0, 360)]
+		public float handOffAngle = 45f;
+
+		[Tooltip("The minimum angle in degrees the wrist has to rotate to be considered a wave")]
+		[Range(0, 180)]
+		public float wristAngle = 60f;
+
+		[Tooltip("The maximum amount of time in seconds allowed to be used to complete a wave")]
+		public float maximumWaveTime = .5f;
+
+		[AutoFind(AutoFindLocations.Parents)]
+		[Tooltip("The hand model to watch. Set automatically if detector is on a hand.")]
+		public IHandModel handModel = null;
+
+		float waveTime = 0;
+
+		private bool isFingerPointingUp = false;
+		private bool areFingersExtended = false;
+		private bool hasBegunWave = false;
+		private bool isWaving = false;
+
+		private PointingState thumb = PointingState.Extended;
+		private PointingState index = PointingState.Extended;
+		private PointingState middle = PointingState.Extended;
+		private PointingState ring = PointingState.Extended;
+		private PointingState pinky = PointingState.Extended;
+
+		private IEnumerator extendedWatcherCoroutine;
+		private IEnumerator fingerWatcherCoroutine;
+
+
+		void OnValidate()
+		{
+			if (handOffAngle < handOnAngle)
+			{
+				handOffAngle = handOnAngle;
+			}
+		}
+
+		void Awake()
+		{
+			extendedWatcherCoroutine = ExtendedFingerWatcher();
+			fingerWatcherCoroutine = FingerPointingWatcher();
+		}
+
+		void Update()
+		{
+			WaveWatcher();
+		}
+
+		void OnEnable()
+		{
+			StartCoroutine(extendedWatcherCoroutine);
+			StartCoroutine(fingerWatcherCoroutine);
+		}
+
+		void OnDisable()
+		{
+			StopCoroutine(extendedWatcherCoroutine);
+			StopCoroutine(fingerWatcherCoroutine);
+			Deactivate();
+		}
+
+		IEnumerator FingerPointingWatcher()
+		{
+			Hand hand;
+			Vector3 fingerDirection;
+			Vector3 targetDirection;
+			while (true)
+			{
+				if (handModel != null)
+				{
+					hand = handModel.GetLeapHand();
+					if (hand != null)
+					{
+						targetDirection = Vector3.up;
+						fingerDirection = hand.Fingers[2].Bone(Bone.BoneType.TYPE_DISTAL).Direction.ToVector3();
+						float angleTo = Vector3.Angle(fingerDirection, targetDirection);
+						if (handModel.IsTracked && angleTo <= handOnAngle)
+						{
+							isFingerPointingUp = true;
+						}
+						else if (!handModel.IsTracked || angleTo >= handOffAngle)
+						{
+							isFingerPointingUp = false;
+						}
+					}
+				}
+				yield return new WaitForSeconds(period);
+			}
+		}
+
+		IEnumerator ExtendedFingerWatcher()
+		{
+			Hand hand;
+			while (true)
+			{
+				bool fingerState = false;
+				if (handModel != null && handModel.IsTracked)
+				{
+					hand = handModel.GetLeapHand();
+					if (hand != null)
+					{
+						fingerState = matchFingerState(hand.Fingers[0], thumb)
+						  && matchFingerState(hand.Fingers[1], index)
+						  && matchFingerState(hand.Fingers[2], middle)
+						  && matchFingerState(hand.Fingers[3], ring)
+						  && matchFingerState(hand.Fingers[4], pinky);
+
+						if (handModel.IsTracked && fingerState)
+						{
+							areFingersExtended = true;
+						}
+						else if (!handModel.IsTracked || !fingerState)
+						{
+							areFingersExtended = false;
+						}
+					}
+				}
+				else if (IsActive)
+				{
+					Deactivate();
+				}
+				yield return new WaitForSeconds(period);
+			}
+		}
+
+		void WaveWatcher()
+		{
+			Hand hand;
+			Vector3 fingerDirection;
+			Vector3 targetDirection;
+			float angleTo;
+
+			if (handModel != null)
+			{
+				hand = handModel.GetLeapHand();
+				if (hand != null)
+				{
+					targetDirection = Vector3.up;
+					fingerDirection = hand.Fingers[2].Bone(Bone.BoneType.TYPE_DISTAL).Direction.ToVector3();
+					angleTo = Vector3.Angle(fingerDirection, targetDirection);
+					if (areFingersExtended && isFingerPointingUp && !hasBegunWave)
+					{
+						hasBegunWave = true;
+						Debug.Log("hasBegunWave = " + hasBegunWave);
+					}
+					if (hasBegunWave && !isFingerPointingUp)
+					{
+						hasBegunWave = false;
+						isWaving = true;
+						Debug.Log("isWaving = " + isWaving);
+					}
+					if (isWaving && angleTo < wristAngle)
+					{
+						waveTime += Time.deltaTime;
+					}
+					else if (isWaving && angleTo >= wristAngle)
+					{
+						waveTime = 0;
+						isWaving = false;
+						Debug.Log("I just did a single wave");
+					}
+				}
+			}
+		}
+
+		private bool matchFingerState(Finger finger, PointingState requiredState)
+		{
+			return (requiredState == PointingState.Either) ||
+				   (requiredState == PointingState.Extended && finger.IsExtended) ||
+				   (requiredState == PointingState.NotExtended && !finger.IsExtended);
+		}
+
+		private enum PointingState { Extended, NotExtended, Either }
+		private enum WaveType { Single, Double}
+	}
+}
